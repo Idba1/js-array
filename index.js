e4c30b3795135cc9123595cdb6c6fa8c5e7dd641
Idba1 <moniraislamself@gmail.com>
2024-01-27 19:47:56 +0600

    inndex.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let number = [11, 3, 333, 66, 66];
+console.log(number.indexOf(333));
+console.log(number [2]);
+
+const second = number[2];
+console.log(second);
+
+number [3] = 11;
+console.log(number);
