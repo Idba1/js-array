ac694efaa56c5089e3daa96bf230d60e062fbedb
Idba1 <moniraislamself@gmail.com>
2024-01-27 20:39:29 +0600

    shift.js

---
+++ shift.js
@@ -0,0 +1,10 @@
+const friend = ['abul', 'babul', 'cabul', 'dabul'];
+console.log(friend);
+
+const out1 = friend.shift();
+console.log(out1);
+
+const out2 = friend.shift();
+console.log(out2);
+
+console.log(friend);
