20915f70d85907d2dd93f33b3c29e6f437edc269
Idba1 <moniraislamself@gmail.com>
2024-01-27 21:53:31 +0600

    includes.js

---
+++ includes.js
@@ -0,0 +1,10 @@
+const friend = ['abul', 'babul', 'cabul', 'dabul'];
+ console.log(friend.includes('hasi'));
+ console.log(friend.includes('babul'));
+
+ if (friend.includes('cabull')){
+    console.log('yes');
+ }
+ else{
+    console.log('no');
+ }
