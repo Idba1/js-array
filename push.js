6e61c8ef3d54f15a7766639fafc78ef6158862df
Idba1 <moniraislamself@gmail.com>
2024-01-27 19:55:35 +0600

    push.js

---
+++ push.js
@@ -0,0 +1,10 @@
+const friend = ['abul', 'babul', 'cabul', 'dabul'];
+
+friend.push('eabul');
+console.log(friend);
+
+friend.push('fabul');
+console.log(friend);
+
+friend.push('gabul');
+console.log(friend);
