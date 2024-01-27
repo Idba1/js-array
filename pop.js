0f8c1fef45f739d812e02c942173ed3fcbfd60c6
Idba1 <moniraislamself@gmail.com>
2024-01-27 20:31:51 +0600

    pop.js

---
+++ pop.js
@@ -0,0 +1,10 @@
+const friend = ['abul', 'babul', 'cabul', 'dabul'];
+console.log(friend);
+
+
+
+const out1 = friend.pop();
+console.log(friend);
+const out2 = friend.pop();
+console.log(friend);
+console.log(out1, out2);
